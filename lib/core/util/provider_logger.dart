e35fc7546411de7b721fcf344fb30ea444c58d75
n1h41 <nihalninu25@gmail.com>
2023-02-28 11:55:01 +0530

    added provider logger, which logs changes made to each providers

---
+++ lib/core/util/provider_logger.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+
+class ProviderLogger extends ProviderObserver {
+  final Logger _logger;
+
+  ProviderLogger(Logger logger) : _logger = logger;
+
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    _logger.i('''
+Provider: "${provider.name ?? provider.runtimeType}"
+PreviousValue: "$previousValue"
+NewValue: "$newValue"
+''');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:chat_app/core/util/provider_logger.dart';
 import 'package:chat_app/features/auth/presentation/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
 void main() {
-  runApp(const MyApp());
+  final Logger logger = Logger();
+  runApp(ProviderScope(
+    observers: [ProviderLogger(logger)],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
